c48322a0ab75677284aaf9ceb265d142a4428e04
salaboy <salaboy@gmail.com>
2020-07-27 13:18:46 +0100

    online store with product service dependency

--- src/main/java/com/salaboy/lke/DemoApplication.java
+++ src/main/java/com/salaboy/lke/DemoApplication.java
@@ -3,9 +3,11 @@ package com.salaboy.lke;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -19,13 +21,23 @@ class SiteController {
     @Value("${version:0.0.0}")
     private String version;
 
+    @Value("${PRODUCT_SERVICE:http://product-service}")
+    private String PRODUCT_SERVICE;
+
+    private RestTemplate restTemplate = new RestTemplate();
+
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("version", version);
+
+        try {
+            ResponseEntity<String> productServiceVersion = restTemplate.getForEntity(PRODUCT_SERVICE + "/info", String.class);
+            model.addAttribute("productService", productServiceVersion.getBody());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return "index";
     }
 
-
-
-
 }
+
--- src/main/resources/templates/index.ftl
+++ src/main/resources/templates/index.ftl
@@ -4,9 +4,9 @@
 <head>
     <title><#if version??>V${version}</#if></title>
 </head>
-<body style="display:flex;align-items:center;justify-content:center;color:#000000;font-family:sans-serif;font-size:6rem;margin:0;letter-spacing:-0.1em">
-<h3>Online Store</h3>
-<h1><#if version??>V${version}</#if></h1>
+<body style="align-items:center;justify-content:center;color:#000000;font-family:sans-serif;font-size:6rem;margin:0;letter-spacing:-0.1em">
+<h2 style="text-align: center">Online Store <#if version??>V${version}</#if></h2>
+<h3 style="text-align: center; color: #0027FA">Product Service <#if productService??>${productService}<#else>N/A</#if></h3>
 </body>
 </html>
 
